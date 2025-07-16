b39e6a812de0cd9612c76b8b291a8b41c21c3d49
Beanmodf <dominhtriet47@gmail.com>
2025-07-16 21:22:53 +0700

    Define domain class for the project

---
+++ fintech.Data/Model/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace fintech.Data.Model
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        [RegularExpression(@"^\d+$", ErrorMessage = "Account number must contain numeric character only")]
+        [MaxLength(20)]
+        [Required]
+        public string AccountNumber { get; set; }
+        [RegularExpression(@"^[a-zA-D\d]+$", ErrorMessage = "Owner name must contain only valid characters")]
+        [MaxLength(20)]
+        [Required]
+        public string OwnerName { get; set; }
+        
+        [Precision(18,3)]
+        [Required]
+        public decimal Balance { get; set; } 
+        [Required]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Phone number must contains numeric character only")]
+        public string PhoneNumber { get; set; }
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
