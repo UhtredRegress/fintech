179133aa3c596190251abd9c7ae8e5ada8578132
Beanmodf <dominhtriet47@gmail.com>
2025-07-17 21:04:04 +0700

    Implement method to create account in database

---
+++ fintech.Data/AccountRepository.cs
@@ -0,0 +1,27 @@
+﻿using fintech.Data.IRepository;
+using fintech.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fintech.Data
+{
+    public class AccountRepository:IAccountRepository
+    {
+        private readonly DbContext _dbContext;
+
+        public AccountRepository(DbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Account> CreateAccount(Account account)
+        {
+            await _dbContext.AddAsync(account);
+            await _dbContext.SaveChangesAsync();
+            return account;
+        }
+    }
+}
---
+++ fintech.Data/IRepository/IAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using fintech.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fintech.Data.IRepository
+{
+    public interface IAccountRepository
+    {
+        Task<Account> CreateAccount(Account account);
+    }
+}
