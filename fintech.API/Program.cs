9a0d6b4e839cf77e537db21ab8b408b0f7d2807c
Beanmodf <dominhtriet47@gmail.com>
2025-07-24 23:12:10 +0700

    feat(server): add seperate port fpr gRPC service with Http2 protocol

--- fintech.API/Program.cs
+++ fintech.API/Program.cs
@@ -1,9 +1,10 @@
 using fintech.Data;
 using fintech.Data.IRepository;
-using fintech.Service.IService;
-using fintech.Service;
-using Microsoft.EntityFrameworkCore;
 using fintech.Data.Model;
+using fintech.Service;
+using fintech.Service.IService;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
 
 namespace fintech.API
 {
@@ -21,10 +22,23 @@ namespace fintech.API
             builder.Services.AddScoped<IAccountService, AccountService>();
 
             builder.Services.AddControllers();
+            builder.Services.AddGrpc();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.ListenAnyIP( 7114, listenOptions =>
+                {
+                    listenOptions.Protocols = HttpProtocols.Http1;
+                });
+
+                options.ListenAnyIP(7001, listenOptions =>
+                {
+                    listenOptions.Protocols = HttpProtocols.Http2;
+                });
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
