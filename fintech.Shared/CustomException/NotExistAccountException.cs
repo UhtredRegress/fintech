fcfbb97d721e6d69d5f7950d230dd03cbcdf8e68
Beanmodf <dominhtriet47@gmail.com>
2025-07-25 13:25:41 +0700

    feat(exception): add NotFoundAccountException for not found account

---
+++ fintech.Shared/CustomException/NotExistAccountException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fintech.Shared.CustomException
+{
+    public class NotExistAccountException : Exception
+    {
+        public NotExistAccountException() { }
+        public NotExistAccountException(string message) : base(message) { }
+        public NotExistAccountException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
